de639de688c48371340b84a850444c7d82bb003e
dmr <mikhail.dubik@gmail.com>
2026-05-10 09:48:20 +0200

    fix the flow

--- news/pipelines/telegram.py
+++ news/pipelines/telegram.py
@@ -2,7 +2,6 @@
 from langgraph.graph import END, START, StateGraph
 
 from news.nodes.telegram_collector import telegram_collector_node
-from news.nodes.telegram_dedup import telegram_dedup_node
 from news.nodes.telegram_extractor import telegram_extract_node
 from news.nodes.telegram_analyzer import telegram_analyze_node
 from news.state import TelegramPipelineState
@@ -11,12 +10,10 @@ from news.state import TelegramPipelineState
 def build_telegram_pipeline():
     builder = StateGraph(TelegramPipelineState)
     builder.add_node("collect", telegram_collector_node)
-    builder.add_node("dedup", telegram_dedup_node)
     builder.add_node("enrich", telegram_extract_node)
     builder.add_node("analyze", telegram_analyze_node)
     builder.add_edge(START, "collect")
-    builder.add_edge("collect", "dedup")
-    builder.add_edge("dedup", "enrich")
+    builder.add_edge("collect", "enrich")
     builder.add_edge("enrich", "analyze")
     builder.add_edge("analyze", END)
     return builder.compile()
